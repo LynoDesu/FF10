b68cea93bb9398398808f77a7c5f680e8382a6a6
Adam Bandeira Lynas <adam@limitbreak.tech>
2019-05-03 21:10:59 +0100

    Added Mac App

---
+++ FF10.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace FF10.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
