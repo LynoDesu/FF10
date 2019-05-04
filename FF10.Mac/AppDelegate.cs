3b47a9478cd34f65b41b1386b303bd375182e13e
Adam Bandeira Lynas <adam@limitbreak.tech>
2019-05-04 17:20:14 +0100

    * AppDelegate.cs: Added Ability to open save files to load them.
    * ViewController.cs: Added method to display character stat values.

--- FF10.Mac/AppDelegate.cs
+++ FF10.Mac/AppDelegate.cs
@@ -1,4 +1,5 @@
 ﻿using AppKit;
+using FF10.Core.Models;
 using Foundation;
 
 namespace FF10.Mac
@@ -13,6 +14,35 @@ namespace FF10.Mac
         public override void DidFinishLaunching(NSNotification notification)
         {
             // Insert code here to initialize your application
+        }
+
+        [Export("openDocument:")]
+        void OpenDialog(NSObject sender)
+        {
+            var dlg = NSOpenPanel.OpenPanel;
+            dlg.CanChooseFiles = true;
+            dlg.CanChooseDirectories = false;
+            dlg.AllowedFileTypes = new string[] { "bin", "" };
+
+            if (dlg.RunModal() == 1)
+            {
+                var path = dlg.Url.Path;
+
+                if (path != null)
+                {
+                    if (SaveData.Instance().Open(path, false) == false)
+                    {
+                        var alert = new NSAlert()
+                        {
+                            AlertStyle = NSAlertStyle.Critical,
+                            InformativeText = "There was a problem trying to load the FFX save file. Please ensure that this is a Nintendo Switch save file.",
+                            MessageText = "Unable to load save",
+                        };
+                        alert.RunModal();
+                        return;
+                    }
+                }
+            }
         }
 
         public override void WillTerminate(NSNotification notification)
--- FF10.Mac/ViewController.cs
+++ FF10.Mac/ViewController.cs
@@ -2,6 +2,7 @@
 
 using AppKit;
 using Foundation;
+using FF10.Core.Models;
 using FF10.Core.ViewModels;
 
 namespace FF10.Mac
@@ -19,6 +20,25 @@ namespace FF10.Mac
             _viewModel = new MainWindowViewModel();
 
             base.ViewDidLoad();
+        }
+
+        void DIsplayCharacterStats(int characterIndex)
+        {
+            txtHP.IntValue = (int)_viewModel.Party[characterIndex].HP;
+            txtMP.IntValue = (int)_viewModel.Party[characterIndex].MP;
+            txtLuck.IntValue = (int)_viewModel.Party[characterIndex].Luck;
+            txtLevel.IntValue = (int)_viewModel.Party[characterIndex].Lv;
+            txtMagic.IntValue = (int)_viewModel.Party[characterIndex].Magic;
+            txtSpeed.IntValue = (int)_viewModel.Party[characterIndex].Speed;
+            txtAttack.IntValue = (int)_viewModel.Party[characterIndex].Attack;
+            txtDefence.IntValue = (int)_viewModel.Party[characterIndex].Defense;
+            txtAccuracy.IntValue = (int)_viewModel.Party[characterIndex].Hit;
+            txtMagicDef.IntValue = (int)_viewModel.Party[characterIndex].MagicGuard;
+            txtAvoidance.IntValue = (int)_viewModel.Party[characterIndex].Avoidance;
+        }
+
+        void SetCharacterListValues()
+        {
         }
 
         public override NSObject RepresentedObject
